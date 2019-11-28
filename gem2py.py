66d2dbef9af9a0ef942578b28662808b9a0c8278
Nathan Wendt <nawendt84@gmail.com>
2019-11-28 15:20:28 -0600

    Updated parser
    Working on file keys

--- gem2py.py
+++ gem2py.py
@@ -4,7 +4,7 @@ import contextlib
 from datetime import datetime
 from enum import Enum
 from io import BytesIO
-from itertools import repeat
+from itertools import cycle, product, repeat
 import logging
 import re
 
@@ -18,6 +18,10 @@ except ImportError:
 
 from metpy.io._tools import Bits, DictStruct, IOBuffer, NamedStruct, open_as_needed
 
+def _word_to_position(word, bytes_per_word=4):
+    r"""Return beginning position of a word in bytes"""
+    return (word * bytes_per_word) - bytes_per_word
+
 
 class GempakDM(AbstractDataStore):
     """
@@ -25,7 +29,7 @@ class GempakDM(AbstractDataStore):
 
     gempak_header = 'GEMPAK DATA MANAGEMENT FILE '
     
-    prod_desc_fmt = DictStruct([('version', 'i'), ('file_headers', 'i'),
+    prod_desc_fmt = NamedStruct([('version', 'i'), ('file_headers', 'i'),
                                 ('file_keys_ptr', 'i'), ('rows', 'i'),
                                 ('row_keys', 'i'), ('row_keys_ptr','i'),
                                 ('row_headers_ptr', 'i'), ('columns', 'i'),
@@ -35,12 +39,7 @@ class GempakDM(AbstractDataStore):
                                 ('data_mgmt_length', 'i'), ('data_block_ptr', 'i'),
                                 ('file_type', 'i'), ('data_source', 'i'),
                                 ('machine_type', 'i'), ('missing_int', 'i'),
-                                ('blank', 'i'), ('blank', 'i'),
-                                ('blank', 'i'),  ('missing_float' ,'f')])
-
-    data_management_fmt = DictStruct([('next_free_word', 'i'), ('max_free_pairs', 'i'),
-                                      ('actual_free_pairs', 'i'), ('last_word', 'i')])
-    
+                                (None, '12x'), ('missing_float' ,'f')])
 
     def __init__(self, filename):
         """
@@ -50,25 +49,31 @@ class GempakDM(AbstractDataStore):
         with contextlib.closing(fobj):
             self._buffer = IOBuffer.fromfile(fobj)
 
+        # Save file start position as pointers use this as reference
         start = self._buffer.set_mark()
 
+        # Process the main GEMPAK header to verify file format
         self._process_gempak_header()
 
-        gpkhd = self._buffer.set_mark()
-
+        # Process main metadata header
         self.prod_desc = self._buffer.read_struct(self.prod_desc_fmt)
+        meta = self._buffer.set_mark()
 
-        self._buffer.jump_to(start, (self.prod_desc['data_mgmt_ptr'] - 1) * 4)
+        # Process file keys
+        # Should this be so general or can we rely on there only ever being NAVB and ANLB?
+        fkey_prod = product(['header_name', 'header_length', 'header_type'], range(1, self.prod_desc.file_headers + 1))
+        fkey_names = [ x for x in [ '{}{}'.format(*x) for x in fkey_prod ] ]
+        fkey_meta = list(zip(fkey_names, np.repeat(('4s', 'i', 'i'), self.prod_desc.file_headers)))
+        self.file_keys_format = NamedStruct(fkey_meta)
 
-        self.dm_header = self._buffer.read_struct(self.data_management_fmt)
+        self._buffer.jump_to(start, _word_to_position(self.prod_desc.file_keys_ptr))
+        self.file_keys = self._buffer.read_struct(self.file_keys_format)
 
-
-        # row metadata
-        # row key 256 (-1)
-        # col key 257 (-1)
-        # part key 267 (-1)
-        
-
+        file_key_blocks = self._buffer.set_mark()
+        # We are now at the NAVB/ANLB portion of the file.
+        for block in range(self.prod_desc.file_headers):
+            bsize = self._buffer.read_int('i')
+            
     def _process_gempak_header(self):
         """Read off the GEMPAK header from the file, if necessary."""
         data = self._buffer.get_next(len(self.gempak_header)).decode('utf-8', 'ignore')
@@ -76,9 +81,3 @@ class GempakDM(AbstractDataStore):
             self._buffer.skip(len(self.gempak_header))
         else:
             raise TypeError('Unknown file format or invalid GEMPAK file')
-
-    def _get_dm_metadata(self):
-        """
-        """
-        self.row_metadata = self._buffer.read_struct()
-
