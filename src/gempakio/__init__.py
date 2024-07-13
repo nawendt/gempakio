f7e0609bfc46955b6982e4ba9ac784dd106c3af9
Nathan Wendt <nathan.wendt@noaa.gov>
2024-07-13 08:05:59 -0500

    Update version

--- src/gempakio/__init__.py
+++ src/gempakio/__init__.py
@@ -7,4 +7,4 @@ from gempakio.decode.gempak import GempakGrid, GempakSounding, GempakSurface
 from gempakio.decode.vgf import VectorGraphicFile
 from gempakio.encode.gempak import GridFile, SoundingFile, SurfaceFile
 
-__version__ = '1.0.2'
+__version__ = '1.1.0'
