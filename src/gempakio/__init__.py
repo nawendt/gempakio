a7de2bfe6708a1b65c0cbb50d024d5d4dbfedd8c
Nathan Wendt <nathan.wendt@noaa.gov>
2026-05-25 10:20:41 -0500

    Bump version/copyright

--- src/gempakio/__init__.py
+++ src/gempakio/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2025 Nathan Wendt.
+# Copyright (c) 2026 Nathan Wendt.
 # Distributed under the terms of the BSD 3-Clause License.
 # SPDX-License-Identifier: BSD-3-Clause
 """gempakIO."""
@@ -7,4 +7,4 @@ from gempakio.decode.gempak import GempakGrid, GempakSounding, GempakSurface
 from gempakio.decode.vgf import VectorGraphicFile
 from gempakio.encode.gempak import GridFile, SoundingFile, SurfaceFile
 
-__version__ = '1.4.3'
+__version__ = '1.4.4'
--- src/gempakio/tools.py
+++ src/gempakio/tools.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2025 Nathan Wendt.
+# Copyright (c) 2026 Nathan Wendt.
 # Copyright (c) 2025,2016,2019 MetPy Developers.
 # Distributed under the terms of the BSD 3-Clause License.
 # SPDX-License-Identifier: BSD-3-Clause
