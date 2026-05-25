c1783696def6423df374e5d378154b55be8fe9e0
Nathan Wendt <nathan.wendt@noaa.gov>
2026-05-25 10:50:19 -0500

    Bump version/copyright (#24)

--- tests/test_surface_encode.py
+++ tests/test_surface_encode.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2025 Nathan Wendt.
+# Copyright (c) 2026 Nathan Wendt.
 # Distributed under the terms of the BSD 3-Clause License.
 # SPDX-License-Identifier: BSD-3-Clause
 """Tests for encoding GEMPAK sounding files."""
